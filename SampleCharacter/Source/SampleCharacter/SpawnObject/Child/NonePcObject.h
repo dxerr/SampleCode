9f3d2da528cfc649781195a301328aef9b4c5633
dxerr <july1981@naver.com>
2019-03-19 15:58:06 +0900

    신규 NPC 리소스 추가

---
+++ SampleCharacter/Source/SampleCharacter/SpawnObject/Child/NonePcObject.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpawnObject/PawnObject.h"
+#include "NonePcObject.generated.h"
+
+class FFSMManager;
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLECHARACTER_API ANonePcObject : public APawnObject
+{
+	GENERATED_BODY()
+	
+};
